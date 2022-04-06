3a80843347a8e1b1ebf1a370483592ea47b6bd0e
Ardiansyah <aardi25@GMAIL.COM>
2022-04-07 00:01:13 +0700

    finish exercise

--- LearnAspBasic/Controllers/MoviesController.cs
+++ LearnAspBasic/Controllers/MoviesController.cs
@@ -12,52 +12,73 @@ namespace LearnAspBasic.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var movies = GetMovies();
+            return View(movies);
         }
-       /* public ActionResult Random()
+
+        private List<Movie> GetMovies()
         {
-            var movie = new Movie() { Name = "Ardiansyah" };
-            var customers = new List<Customer>
+            return new List<Movie>
             {
-                new Customer { Name = "Customer 1"},
-                new Customer {Name = "Customer 2"},
+                new Movie
+                {
+                   Id = 1,
+                   Name = "Jaka Tingkir",
+                },
+                new Movie
+                {
+                    Id = 2,
+                    Name = "Mak Lampir"
+                }
             };
-
-            var viewModel = new RandomMovieViewModel()
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(viewModel);
- 
         }
 
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
 
-            if (string.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
-            return Content(String.Format("pageindex={0}&sortBy={1}",pageIndex,sortBy));
-        }
+        /* public ActionResult Random()
+         {
+             var movie = new Movie() { Name = "Ardiansyah" };
+             var customers = new List<Customer>
+             {
+                 new Customer { Name = "Customer 1"},
+                 new Customer {Name = "Customer 2"},
+             };
 
-        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }*/
+             var viewModel = new RandomMovieViewModel()
+             {
+                 Movie = movie,
+                 Customers = customers
+             };
 
-        
+             return View(viewModel);
+
+         }
+
+         public ActionResult Edit(int id)
+         {
+             return Content("id=" + id);
+         }
+
+         public ActionResult Index(int? pageIndex, string sortBy)
+         {
+             if (!pageIndex.HasValue)
+             {
+                 pageIndex = 1;
+             }
+
+             if (string.IsNullOrWhiteSpace(sortBy))
+             {
+                 sortBy = "Name";
+             }
+             return Content(String.Format("pageindex={0}&sortBy={1}",pageIndex,sortBy));
+         }
+
+         [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+         public ActionResult ByReleaseDate(int year, int month)
+         {
+             return Content(year + "/" + month);
+         }*/
+
+
     }
 }
